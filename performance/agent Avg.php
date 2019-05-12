bd9d4d7592748cfd2db6bcb6b703f03170dfd760
jonashle <50541028+jonashle@users.noreply.github.com>
2019-05-12 20:21:09 +0200

    Add files via upload
    Performance sqls

---
+++ performance/agent Avg.php
@@ -0,0 +1,50 @@
+<?php
+$year;
+if (isset($_GET['year'])) {
+    $year= $_GET['year'];
+} else {
+    $year=2018;
+}
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include("../connection.php");
+
+$sql = "SELECT AVG(sales.Sale_Amount) AS AvSaleAmount,AVG(sales.Sales_TimeOnMarket) AS AvTime, person.Person_Firstname,person.Person_LastName
+FROM (agent
+INNER JOIN sales
+ON agent.Agent_ID=sales.Agent_Agent_ID)
+INNER JOIN person 
+ON person.Person_ID=agent.Agent_ID
+WHERE YEAR(sales.Sale_Date)=".$year."
+GROUP BY agent.Agent_ID
+";
+
+echo $sql;
+$result = $conn->query($sql);
+$jsonarray=array();
+if ($result->num_rows > 0) {
+    // output data of each row
+	while($arrayresult = mysqli_fetch_array($result)) {
+       $myArray[] = array( 
+						"Agent_FirstName"=>$arrayresult['Person_Firstname'],
+						"Agent_LastName"=>$arrayresult['Person_LastName'],
+						"AvSaleAmount"=>$arrayresult['AvSaleAmount'],
+						"AvTime"=>$arrayresult['AvTime']
+                                           );
+    }
+	// set response code - 200 OK
+    http_response_code(200);
+
+    echo json_encode($myArray);
+} else {
+// set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No properties found.")
+    );
+}
+$conn->close();
+?>
---
+++ performance/listing suburb.php
@@ -0,0 +1,53 @@
+<?php
+$year;
+if (isset($_GET['city'])) {
+    $city= $_GET['city'];
+} else {
+    $city="Cape Town";
+}
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include("../connection.php");
+
+$sql = "SELECT suburb.suburb_Name, COUNT(*) AS Amount
+FROM ((((available
+INNER JOIN property
+On available.Property_ID =property.Prop_ID)
+INNER JOIN address
+ON address.Address_ID=property.Address_Address_ID)
+INNER JOIN street
+ON street.Street_ID=address.Address_ID)
+INNER JOIN suburb
+ON suburb.Suburb_ID=street.Suburb_Suburb_ID)
+INNER JOIN city
+ON city.City_ID=suburb.City_ID
+WHERE city_Name= \"".$city."\"
+GROUP BY suburb.suburb_ID";
+
+echo $sql;
+$result = $conn->query($sql);
+$jsonarray=array();
+if ($result->num_rows > 0) {
+    // output data of each row
+	while($arrayresult = mysqli_fetch_array($result)) {
+       $myArray[] = array( 
+						"Suburb_Name"=>$arrayresult['suburb_Name'],
+						"Amount"=>$arrayresult['Amount'],		
+                                           );
+    }
+	// set response code - 200 OK
+    http_response_code(200);
+
+    echo json_encode($myArray);
+} else {
+// set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No properties found.")
+    );
+}
+$conn->close();
+?>
